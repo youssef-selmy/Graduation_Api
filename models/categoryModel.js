9a289904eb7200be7aa7a4ed6bc0c8673f60f33c
ra7ma3atef <151402363+ra7ma3atef@users.noreply.github.com>
2024-02-25 00:37:52 +0200

    Add files via upload

---
+++ models/categoryModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+// 1- Create Schema
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Category required'],
+      unique: [true, 'Category must be unique'],
+      minlength: [3, 'Too short category name'],
+      maxlength: [32, 'Too long category name'],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    image: String,
+  },
+  { timestamps: true }
+);
+
+
+// 2- Create model
+const CategoryModel = mongoose.model('Category', categorySchema);
+
+module.exports = CategoryModel;
