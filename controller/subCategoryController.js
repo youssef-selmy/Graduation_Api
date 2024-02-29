d8882f3acce165f67a7ca71ff5545be1d303a69f
ra7ma3atef <151402363+ra7ma3atef@users.noreply.github.com>
2024-03-01 00:57:19 +0200

    Update subCategoryController.js

--- controller/subCategoryController.js
+++ controller/subCategoryController.js
@@ -1,4 +1,4 @@
-const factory = require('./handlersFactoryController');
+const factory = require('./handlersFactory');
 const SubCategory = require('../models/subCategoryModel');
 
 exports.setCategoryIdToBody = (req, res, next) => {
@@ -8,7 +8,7 @@ exports.setCategoryIdToBody = (req, res, next) => {
 };
 
 // Nested route
-// GET /api/v1/category/:categoryId/subcategory
+// GET /api/v1/categories/:categoryId/subcategories
 exports.createFilterObj = (req, res, next) => {
   let filterObject = {};
   if (req.params.categoryId) filterObject = { category: req.params.categoryId };
@@ -16,22 +16,27 @@ exports.createFilterObj = (req, res, next) => {
   next();
 };
 
-// Get list of subcategories
-// GET /api/v1/subcategory
+//  Get list of subcategories
+//  GET /api/v1/subcategories
+
 exports.getSubCategories = factory.getAll(SubCategory);
 
-// Get specific subcategory by id
-// GET /api/v1/subcategory/:id
+//  Get specific subcategory by id
+//  GET /api/v1/subcategories/:id
+
 exports.getSubCategory = factory.getOne(SubCategory);
 
-// Create subCategory
-// POST  /api/v1/subcategory
+//  Create subCategory
+//  POST  /api/v1/subcategories
+
 exports.createSubCategory = factory.createOne(SubCategory);
 
-// Update specific subcategory
-// PUT /api/v1/subcategory/:id
+//  Update specific subcategory
+//  PUT /api/v1/subcategories/:id
+
 exports.updateSubCategory = factory.updateOne(SubCategory);
 
 // Delete specific subCategory
-// DELETE /api/v1/subcategory/:id
+//  DELETE /api/v1/subcategories/:id
+
 exports.deleteSubCategory = factory.deleteOne(SubCategory);
