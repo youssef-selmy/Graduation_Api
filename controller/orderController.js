cba5fff800e683475a50bb4383122539fa7d84c8
youssef_selmy <ym79793@gmail.com>
2024-03-22 04:49:54 +0200

    stripe updates

--- controller/orderController.js
+++ controller/orderController.js
@@ -143,9 +143,17 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
-        name: req.user.name,
-        amount: totalOrderPrice * 100,
-        currency: 'egp',
+        price_data:{
+          amount: totalOrderPrice * 100,
+          currency: 'egp',
+          product_data: {
+            name: req.user.name,
+          
+          },
+        },
+        // name: req.user.name,
+        // amount: totalOrderPrice * 100,
+        // currency: 'egp',
         quantity: 1,
       },
     ],
